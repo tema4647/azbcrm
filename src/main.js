6784c74b7bc52e48f74eaeef86ce868792f46e2c
artem <tema4647@yandex.ru>
2022-05-19 11:09:15 +0500

    ferst commit

--- src/main.js
+++ src/main.js
@@ -2,5 +2,16 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import PrimeVue from 'primevue/config';
+import Button from 'primevue/button'
 
-createApp(App).use(store).use(router).mount('#app')
+import 'primevue/resources/themes/saga-blue/theme.css'       //theme
+import 'primevue/resources/primevue.min.css'                 //core css
+import 'primeicons/primeicons.css'                           //icons
+
+const app = createApp(App)
+app.use(store)
+app.use(router)
+app.use(PrimeVue)
+app.component('Button', Button)
+app.mount('#app')
---
+++ src/views/Clients.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <Button icon='pi pi-user'></Button>
+  </div>
+</template>
+
+<script>
+  export default {
+    name:'Clients',
+
+    components:{
+    }
+    
+  }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
